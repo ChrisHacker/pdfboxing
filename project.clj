f5555a7b5c3fe952468ed30c49cdd7ce2ff7c22c
Chris <chris+github@shorttrack.io>
2018-12-06 14:31:26 -0600

    Bump pdfbox to 2.0.13

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject pdfboxing "0.1.14.1-SNAPSHOT"
   :description "Clojure PDF manipulation library & wrapper for PDFBox"
-  :url "https://github.com/dotemacs/pdfboxing"
+  :url "https://github.com/ChrisHacker/pdfboxing"
   :license {:name "BSD"
             :url "http://www.opensource.org/licenses/bsd-license"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.apache.pdfbox/pdfbox "2.0.11"]])
+                 [org.apache.pdfbox/pdfbox "2.0.13"]])
